3e16d7fe538e09f07d4dbee3d786a7cc8afcaf3c
Mason <mirabito.mason@gmail.com>
2026-07-22 21:06:29 +0930

    add note after experimentation with groups to add widgets to

--- extension.js
+++ extension.js
@@ -4,204 +4,182 @@
 */
 "use strict";
 
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
-import GLib from 'gi://GLib';
-import St from 'gi://St';
-import Clutter from 'gi://Clutter';
-import Gio from 'gi://Gio'
-import WidgetRegistry from './WidgetRegistry.js';
-import WidgetLayout from './WidgetLayout.js';
+import * as Main from "resource:///org/gnome/shell/ui/main.js";
+import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
+import GLib from "gi://GLib";
+import St from "gi://St";
+import Clutter from "gi://Clutter";
+import Gio from "gi://Gio";
+import WidgetRegistry from "./WidgetRegistry.js";
+import WidgetLayout from "./WidgetLayout.js";
 
 export default class DesktopWidgetsExtension extends Extension {
-    constructor(metadata) {
-        super(metadata);
+  constructor(metadata) {
+    super(metadata);
 
-        this.grid = null;
-        this.settings = null;
-        this.widget_registry = null;
-        this.widget_layout = null;
+    this.grid = null;
+    this.settings = null;
+    this.widget_registry = null;
+    this.widget_layout = null;
 
-        this._monitorChangedId = null;
-        this._settingsChangedId = null;
+    this._monitorChangedId = null;
+    this._settingsChangedId = null;
 
-        this._enabled = false;
-        this._buildGeneration = 0;
-    }
+    this._enabled = false;
+    this._buildGeneration = 0;
+  }
 
-    enable() {
-        this._enabled = true;
-        this._buildGeneration++;
+  enable() {
+    this._enabled = true;
+    this._buildGeneration++;
 
-        this.settings = this.getSettings();
-        this.widget_registry = new WidgetRegistry(
-            this.path,
-            this.settings
-        );
-        this.widget_layout = new WidgetLayout(this.settings);
+    this.settings = this.getSettings();
+    this.widget_registry = new WidgetRegistry(this.path, this.settings);
+    this.widget_layout = new WidgetLayout(this.settings);
 
-        this._monitorChangedId = Main.layoutManager.connect(
-            'monitors-changed',
-            () => {
-                this.buildGrid().catch(console.error);
-            }
-        );
-
-        this._settingsChangedId = this.settings.connect(
-            'changed',
-            () => {
-                this.buildGrid().catch(console.error);
-            }
-        );
-
+    this._monitorChangedId = Main.layoutManager.connect(
+      "monitors-changed",
+      () => {
         this.buildGrid().catch(console.error);
+      },
+    );
+
+    this._settingsChangedId = this.settings.connect("changed", () => {
+      this.buildGrid().catch(console.error);
+    });
+
+    this.buildGrid().catch(console.error);
+  }
+
+  disable() {
+    this._enabled = false;
+    this._buildGeneration++;
+
+    if (this._settingsChangedId) {
+      this.settings.disconnect(this._settingsChangedId);
+      this._settingsChangedId = null;
     }
 
-    disable() {
-        this._enabled = false;
-        this._buildGeneration++;
-
-        if (this._settingsChangedId) {
-            this.settings.disconnect(this._settingsChangedId);
-            this._settingsChangedId = null;
-        }
-
-        if (this._monitorChangedId) {
-            Main.layoutManager.disconnect(this._monitorChangedId);
-            this._monitorChangedId = null;
-        }
-
-        if (this.grid) {
-            this.grid.destroy();
-            this.grid = null;
-        }
-
-        this.settings = null;
-        this.widget_registry = null;
-        this.widget_layout = null;
+    if (this._monitorChangedId) {
+      Main.layoutManager.disconnect(this._monitorChangedId);
+      this._monitorChangedId = null;
     }
 
+    if (this.grid) {
+      this.grid.destroy();
+      this.grid = null;
+    }
 
-    async buildGrid() {
-        console.log("BUILD GRID CALLED");
-        if (!this._enabled)
-            return;
+    this.settings = null;
+    this.widget_registry = null;
+    this.widget_layout = null;
+  }
 
-        const generation = this._buildGeneration;
+  async buildGrid() {
+    console.log("BUILD GRID CALLED");
+    if (!this._enabled) return;
 
+    const generation = this._buildGeneration;
 
-        const outer_spacing = this.settings.get_int("outer-spacing");
-        const inner_spacing = this.settings.get_int("inner-spacing");
-        const widget_size = this.settings.get_int("widget-size");
+    const outer_spacing = this.settings.get_int("outer-spacing");
+    const inner_spacing = this.settings.get_int("inner-spacing");
+    const widget_size = this.settings.get_int("widget-size");
 
+    let work_area = Main.layoutManager.getWorkAreaForMonitor(
+      Main.layoutManager.primaryIndex,
+    );
+    let columns = Math.floor(work_area.width / (widget_size + inner_spacing));
 
+    let rows = Math.floor(work_area.height / (widget_size + inner_spacing));
 
-        let work_area = Main.layoutManager.getWorkAreaForMonitor(
-            Main.layoutManager.primaryIndex
-        );
-        let columns = Math.floor(
-            work_area.width / (widget_size + inner_spacing)
-        );
+    /*  */
+    let new_grid;
 
-        let rows = Math.floor(
-            work_area.height / (widget_size + inner_spacing)
-        );
+    new_grid = new St.Widget({
+      layout_manager: new Clutter.GridLayout(),
+    });
+    new_grid.set_position(
+      work_area.x + outer_spacing,
+      work_area.y + outer_spacing,
+    );
 
+    new_grid.set_size(
+      columns * widget_size + (columns - 1) * inner_spacing,
+      rows * widget_size + (rows - 1) * inner_spacing,
+    );
 
-        /*  */
-        let new_grid;
+    let layout = new_grid.layout_manager;
+    layout.set_column_homogeneous(true);
+    layout.set_row_homogeneous(true);
+    layout.set_column_spacing(inner_spacing);
+    layout.set_row_spacing(inner_spacing);
 
-        new_grid = new St.Widget({
-            layout_manager: new Clutter.GridLayout(),
+    const active_widgets = this.widget_layout.GetActive();
+    console.log(`active: ${JSON.stringify(active_widgets, null, 2)}`);
+
+    for (const widget of active_widgets) {
+      console.log(`Requested widget ID: ${widget.id}`);
+      console.log(`Registry IDs: ${this.widget_registry.list().join(", ")}`);
+
+      const widget_from_reg = this.widget_registry.get(widget.id);
+      console.log(widget_from_reg);
+
+      const widget_class = await this.widget_registry.load(
+        widget_from_reg.metadata.id,
+      );
+      let widget_instance = new widget_class(widget_size, widget_size);
+
+      widget_instance.set_size(widget_size, widget_size);
+      widget_instance.set_x_expand(false);
+      widget_instance.set_y_expand(false);
+      widget_instance.set_x_align(Clutter.ActorAlign.CENTER);
+      widget_instance.set_y_align(Clutter.ActorAlign.CENTER);
+      let x = Math.min(widget.x, columns - 1);
+      let y = Math.min(widget.y, rows - 1);
+
+      layout.attach(widget_instance, x, y, widget.w, widget.h);
+    }
+    for (let i = 0; i < columns; i++) {
+      for (let j = 0; j < rows; j++) {
+        const isSpaceTaken = active_widgets.some((w) => {
+          let clampedX = Math.min(w.x, columns - 1);
+          let clampedY = Math.min(w.y, rows - 1);
+          return clampedX === i && clampedY === j;
         });
-        new_grid.set_position(
-            work_area.x + outer_spacing,
-            work_area.y + outer_spacing
-        );
 
-        new_grid.set_size(
-            columns * widget_size + (columns - 1) * inner_spacing,
-            rows * widget_size + (rows - 1) * inner_spacing
-        );
-
-        let layout = new_grid.layout_manager;
-        layout.set_column_homogeneous(true);
-        layout.set_row_homogeneous(true);
-        layout.set_column_spacing(inner_spacing);
-        layout.set_row_spacing(inner_spacing);
-
-
-
-        const active_widgets = this.widget_layout.GetActive();
-        console.log(`active: ${JSON.stringify(active_widgets, null, 2)}`)
-
-
-        for (const widget of active_widgets) {
-            console.log(`Requested widget ID: ${widget.id}`);
-            console.log(`Registry IDs: ${this.widget_registry.list().join(", ")}`)
-
-            const widget_from_reg = this.widget_registry.get(widget.id);
-            console.log(widget_from_reg);
-
-
-            const widget_class = await this.widget_registry.load(widget_from_reg.metadata.id);
-            let widget_instance = new widget_class(widget_size, widget_size)
-
-            widget_instance.set_size(widget_size, widget_size);
-            widget_instance.set_x_expand(false);
-            widget_instance.set_y_expand(false);
-            widget_instance.set_x_align(Clutter.ActorAlign.CENTER);
-            widget_instance.set_y_align(Clutter.ActorAlign.CENTER);
-            let x = Math.min(widget.x, columns - 1);
-            let y = Math.min(widget.y, rows - 1);
-
-            layout.attach(
-                widget_instance,
-                x,
-                y,
-                widget.w,
-                widget.h
-            );
+        if (!isSpaceTaken) {
+          const empty = new St.Widget({
+            // uncomment for debug
+            // width: 300,
+            // height: 300,
+            // style: 'background-color: red;',
+            // x_expand: false,
+            // y_expand: false,
+            // x_align: Clutter.ActorAlign.CENTER,
+            // y_align: Clutter.ActorAlign.CENTER
+          });
+          layout.attach(empty, i, j, 1, 1);
         }
-        for (let i = 0; i < columns; i++) {
-            for (let j = 0; j < rows; j++) {
-
-                const isSpaceTaken = active_widgets.some(w => {
-                    let clampedX = Math.min(w.x, columns - 1);
-                    let clampedY = Math.min(w.y, rows - 1);
-                    return clampedX === i && clampedY === j;
-                });
-
-                if (!isSpaceTaken) {
-                    const empty = new St.Widget({
-                        // uncomment for debug
-                        // width: 300,
-                        // height: 300,
-                        // style: 'background-color: red;',
-                        // x_expand: false,
-                        // y_expand: false,
-                        // x_align: Clutter.ActorAlign.CENTER,
-                        // y_align: Clutter.ActorAlign.CENTER
-                    });
-                    layout.attach(empty, i, j, 1, 1);
-                }
-            }
-        }
-
-        if (!this._enabled ||
-            generation !== this._buildGeneration) {
-            new_grid.destroy();
-            return;
-        }
-
-        if (this.grid) {
-            this.grid.destroy();
-        }
-
-
-        this.grid = new_grid;
-        Main.layoutManager._backgroundGroup.add_child(this.grid);
-        console.log("DESKTOP WIDGET ENABLE CALLED");
+      }
     }
 
+    if (!this._enabled || generation !== this._buildGeneration) {
+      new_grid.destroy();
+      return;
+    }
+
+    if (this.grid) {
+      this.grid.destroy();
+    }
+
+    this.grid = new_grid;
+
+    Main.layoutManager._backgroundGroup.add_child(this.grid);
+    /* inside the meta window group it will disapear for a fraction of a second on workspace 
+    change i think due to the way mutter and clutter render application windows alongside the 
+    wallpaper and any other windows. however the wallpaper is also in the meta window group, 
+    so we cannot add it below the meta window group. and adding it above the meta window group 
+    is above the application windows which we do not want.*/
+    console.log("DESKTOP WIDGET ENABLE CALLED");
+  }
 }
